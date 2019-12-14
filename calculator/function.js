7e0bfa9530f1b866e9713c4e9f3fee20310c812d
NicholasBurness <nickburness93@outlook.com>
2019-12-14 18:19:18 +0000

    calculator scaffold

---
+++ calculator/function.js
@@ -0,0 +1,23 @@
+// DOM objects 
+
+
+// Variables
+
+
+// Functions 
+// basic scaffold
+const addition = () => {
+    return number + number;
+}
+
+const subtraction = () => {
+    return number - number;
+}
+
+const multiplication = () => {
+    return number * number;
+}
+
+const division = () => {
+    return number / number;
+}
