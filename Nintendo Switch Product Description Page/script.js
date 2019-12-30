b489e5bd86f98f1548eafe8186b0a2e2538f7aec
NicholasBurness <nickburness93@outlook.com>
2019-12-30 11:18:15 +0000

    apply javascript function

---
+++ Nintendo Switch Product Description Page/script.js
@@ -0,0 +1,17 @@
+function openPane(event) {
+    let activePane = document.querySelectorAll(".active");
+
+    activePane.forEach(function (tab) {
+        tab.className = tab.className.replace("active", "");
+    });
+
+    // activate new tab and display panel
+    event.target.parentElement.className += " active";
+    document.getElementById(event.target.href.split("#")[1]).className += " active";
+}
+
+const element = document.getElementById("button-tab");
+
+element.addEventListener("click", openPane, false);
+
+console.log(openPane);
